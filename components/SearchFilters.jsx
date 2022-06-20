721a827d9d8efcb1515bf6eddcceb75a090e0385
Andreas Vieten <cryptonari@outlook.com>
2022-06-20 20:49:01 +0200

    add: SearchFilter Component

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { Flex, Select, Box, Text, Input, Spinner, Icon, Button } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+import { MdCancel } from 'react-icons/md'
+
+
+
+const SearchFilters = () => {
+  return (
+    <div>SearchFilters</div>
+  )
+}
+
+export default SearchFilters
